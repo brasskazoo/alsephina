517afb9bd0bf1b9ab931cd7d32fe84608a7c4422
Will Dampney <will@brasskazoo.com>
2021-03-01 15:46:51 +1000

    Simplify System class to only data

--- src/System/System.test.ts
+++ src/System/System.test.ts
@@ -1,14 +1,6 @@
 import StarSystem from './System';
 
-test('can create a star system with default values', () => {
-    let system = new StarSystem();
+test('can create a star system with default name', () => {
+    const system = new StarSystem();
     expect(system).toHaveProperty('name');
-    expect(system).toHaveProperty('explored', false);
-});
-
-test('can explore a system', () => {
-    let system = new StarSystem();
-
-    system.explore();
-    expect(system.explored).toBe(true);
 });
--- src/System/System.ts
+++ src/System/System.ts
@@ -1,20 +1,12 @@
 interface ISystem {
     name: string;
-    explored: boolean;
     // occupied: boolean
 }
 
 export default class StarSystem implements ISystem {
     name: string;
-    explored: boolean;
 
     constructor() {
         this.name = 'system' + Math.floor(100 * Math.random());
-        this.explored = false;
-    }
-
-    explore(): void {
-        console.debug(`Exploring: ${this.name}`);
-        this.explored = true;
     }
 }

4c766db5fd169efef075dbd9c275b4946aab632e
Will Dampney <will@brasskazoo.com>
2021-03-01 11:12:26 +1000

    Handle exploring systems and display explored systems

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import React, { ReactElement } from 'react';
+import React, { useState } from 'react';
 import PlayerComponent from './Player/PlayerComponent';
 import Player from './Player/Player';
 
 const player = new Player();
 
-function App(): ReactElement {
-    console.log(React.version); // This is  a hack
+function App(): React.ReactElement {
+    const handleExploreSystem = (id: string) => {
+        const targetSystem = thePlayer.knownSystems.find((item) => id === item.name);
+
+        const updatedSystems = thePlayer.knownSystems.map((sys) => {
+            if (sys == targetSystem) {
+                sys.explore();
+            }
+            return sys;
+        });
+
+        updatePlayer({ ...thePlayer, knownSystems: updatedSystems });
+    };
+
+    const [thePlayer, updatePlayer] = useState(player);
+
     return (
         <div className="App">
-            <PlayerComponent player={player} />
+            <PlayerComponent player={player} exploreSystem={handleExploreSystem} />
         </div>
     );
 }
--- src/Player/PlayerComponent.tsx
+++ src/Player/PlayerComponent.tsx
@@ -3,11 +3,21 @@ import Player from './Player';
 
 type Props = {
     player: Player;
+    exploreSystem: (id: string) => void;
 };
 
-const PlayerComponent: React.FC<Props> = ({ player }: Props) => {
-    // let unexploredSystems: StarSystem[] = player.knownSystems;
+const PlayerComponent: React.FC<Props> = ({ player, exploreSystem }: Props) => {
     const playerSystemName = player.homeSystem.name;
+
+    const exploredSystems = player.knownSystems
+        .filter((sys) => {
+            return sys.explored === true;
+        })
+        .map((sys) => (
+            <div key={sys.name}>
+                <h3>{sys.name}</h3>
+            </div>
+        ));
 
     const unexploredSystems = player.knownSystems
         .filter((sys) => {
@@ -16,7 +26,7 @@ const PlayerComponent: React.FC<Props> = ({ player }: Props) => {
         .map((sys) => (
             <div key={sys.name}>
                 <h3>{sys.name}</h3>
-                <button>Explore...</button>
+                <button onClick={() => exploreSystem(sys.name)}>Explore...</button>
             </div>
         ));
 
@@ -30,6 +40,10 @@ const PlayerComponent: React.FC<Props> = ({ player }: Props) => {
             <fieldset>
                 <legend>Unexplored Systems</legend>
                 {unexploredSystems}
+            </fieldset>
+            <fieldset>
+                <legend>Explored Systems</legend>
+                {exploredSystems}
             </fieldset>
         </div>
     );

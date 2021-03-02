99e8906d2badbda87962f09ceb363117fc732031
Will Dampney <will@brasskazoo.com>
2021-03-02 23:30:34 +1000

    Introduce basic SystemEnvironment enum

--- src/System/System.test.ts
+++ src/System/System.test.ts
@@ -1,6 +1,18 @@
-import StarSystem from './System';
+import StarSystem, { SystemEnvironment } from './System';
 
 test('can create a star system with default name', () => {
     const system = new StarSystem();
     expect(system).toHaveProperty('name');
+});
+
+test('star system has an environment suitability', () => {
+    const system = new StarSystem();
+    expect(system).toHaveProperty('environment');
+    expect(system.environment).not.toBeNull();
+});
+
+test('star system can set an environment suitability', () => {
+    const system = new StarSystem(SystemEnvironment.SUITABLE);
+    expect(system).toHaveProperty('environment');
+    expect(system.environment).toEqual(SystemEnvironment.SUITABLE);
 });
--- src/System/System.ts
+++ src/System/System.ts
@@ -1,12 +1,29 @@
+export enum SystemEnvironment {
+    NOT_HABITABLE,
+    HOSTILE,
+    SUITABLE,
+}
+
 interface ISystem {
     name: string;
-    // occupied: boolean
+    environment: SystemEnvironment;
 }
 
 export default class StarSystem implements ISystem {
     name: string;
+    environment: SystemEnvironment;
 
-    constructor() {
+    constructor(environment?: SystemEnvironment) {
         this.name = 'system' + Math.floor(100 * Math.random());
+        this.environment = environment ? environment : randomEnum(SystemEnvironment);
     }
+}
+
+function randomEnum<T>(anEnum: T): T[keyof T] {
+    const enumValues = (Object.keys(anEnum)
+        .map((n) => Number.parseInt(n))
+        .filter((n) => !Number.isNaN(n)) as unknown) as T[keyof T][];
+    const randomIndex = Math.floor(Math.random() * enumValues.length);
+    const randomEnumValue = enumValues[randomIndex];
+    return randomEnumValue;
 }

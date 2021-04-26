0cf04974f4af31ee910c7554313c6793de00b7d3
Will Dampney <will@brasskazoo.com>
2021-04-26 18:30:42 +1000

    Update Player class and begin implementing tests for new event structure

--- src/Player/Player.test.ts
+++ src/Player/Player.test.ts
@@ -1,108 +1,182 @@
-import StarSystem, { SystemEnvironment } from '../System/StarSystem';
-import Player from './Player';
+import ServiceFactory from '../services/ServiceFactory';
+import { default as StarSystem, SystemEnvironment } from '../System/StarSystem';
+import Player, { PlayerType } from './Player';
+import { generateId } from '../util/EntityId';
+import postal from 'postal';
 
-test('New Player has an id', () => {
-    const player = new Player();
-
-    expect(player.id).not.toBeNull();
+beforeAll(() => {
+    ServiceFactory();
 });
 
-test('Player start with a colonised home system', () => {
-    const player = new Player();
+describe('New player object', () => {
+    let player: PlayerType;
 
-    expect(player.homeSystemId).not.toBeNull();
-    expect(player.colonisedSystems).toHaveLength(1);
-    expect(player.colonisedSystems[0].id).toEqual(player.homeSystemId);
-    expect(player.colonisedSystems[0].colony).not.toBeNull();
-});
+    beforeAll(() => {
+        player = new Player();
+    });
 
-test('Player has unexplored systems', () => {
-    const player = new Player();
+    test('player is correct type', () => {
+        expect(player).toBeInstanceOf(Player)
+    });
 
-    expect(player).toHaveProperty('visibleSystems');
-    expect(player.visibleSystems).toHaveLength(3);
-});
-
-test('Player can explore a system', () => {
-    const player = new Player();
-
-    const visibleSystemsCount = player.visibleSystems.length;
-
-    player.explore(player.visibleSystems[0].id);
-
-    expect(player.exploredSystems).toHaveLength(1);
-    expect(player.visibleSystems).not.toContain(player.exploredSystems[0]);
-    expect(player.visibleSystems.length).toBeLessThan(visibleSystemsCount + 4);
-});
-
-test('Player cannot explore an unknown system', () => {
-    const player = new Player();
-    const sys = new StarSystem();
-
-    const block = () => {
-        player.explore(sys.id);
-    };
-    expect(block).toThrowError(`Cannot explore system ${sys.id}: Could not find matching visible system`);
-});
-
-test('Player can colonise suitable environments', () => {
-    const player = new Player();
-
-    expect(player.canColonise(SystemEnvironment.SUITABLE)).toBe(true);
-    expect(player.canColonise(SystemEnvironment.NOT_HABITABLE)).toBe(false);
-    expect(player.canColonise(SystemEnvironment.HOSTILE)).toBe(false);
-});
-
-test('Player can colonise a system', () => {
-    const player = new Player();
-
-    const sys = new StarSystem(SystemEnvironment.SUITABLE);
-    player.exploredSystems.push(sys);
-
-    player.colonise(sys.id);
-    expect(player.colonisedSystems).toHaveLength(2);
-});
-
-test('Player cannot colonise an unsuitable system', () => {
-    const player = new Player();
-    const sys = new StarSystem(SystemEnvironment.HOSTILE);
-
-    const block = () => {
-        player.exploredSystems.push(sys);
-        player.colonise(sys.id);
-    };
-    expect(block).toThrowError(`Cannot colonise system ${sys.id}: Not a suitable environment`);
+    test('New Player has an id', () => {
+        expect(player.id).not.toBeNull();
+    });
 });
 
 /**
- * Player can only colonise systems that exist in their explored list
+ * Creating a new player triggers a sequence of events. These tests should check the outcome.
  */
-test('Player cannot colonise an unknown system', () => {
-    const player = new Player();
-    const sys = new StarSystem(SystemEnvironment.HOSTILE);
+describe('New player event outcome', () => {
+    const reqId = generateId();
+    let player: PlayerType;
 
-    const block = () => {
-        player.colonise(sys.id);
-    };
-    expect(block).toThrowError(`Cannot colonise system ${sys.id}: Could not find matching explored system`);
+    test('Player is created by an event', (done) => {
+        // Setup subscriptions first
+        postal.subscribe({
+            channel: 'player', topic: 'player.updated', callback: ((data, _env) => {
+                expect(data.reqId).toEqual(reqId);
+                expect(data.player).toBeDefined();
+
+                player = data.player;
+
+                done();
+            })
+        });
+
+        // Trigger player.create event
+        postal.publish({channel: 'player', topic: 'create.new', data: { reqId }});
+    });
+
+    test('Player start with a colonised home system', () => {
+        expect(player.homeSystemId).not.toBeNull();
+        expect(player.colonies).toHaveLength(1);
+
+        const homeColony = player.colonies[0];
+        expect(homeColony.systemId).toEqual(player.homeSystemId);
+
+        const homeSystem = player.exploredSystems.find(s => (s.id === player.homeSystemId));
+        // TODO see StarSystemService expect(homeSystem?.colonyId).toEqual(homeColony.id)
+        expect(homeSystem).toBeDefined();
+    });
+
+    test('Player has unexplored systems', () => {
+        expect(player).toHaveProperty('visibleSystems');
+        expect(player.visibleSystems).toHaveLength(3);
+    });
 });
 
-test('Player cannot colonise an unexplored system', () => {
-    const player = new Player();
-    const sys = player.visibleSystems[0];
+describe.skip('Player exploration', () => {
+    const reqId = generateId();
+    let player: PlayerType;
 
-    const block = () => {
-        player.colonise(sys.id);
-    };
-    expect(block).toThrowError(`Cannot colonise system ${sys.id}: Could not find matching explored system`);
+    beforeAll((done) => {        // Setup subscriptions first
+        postal.subscribe({
+            channel: 'player', topic: 'player.updated', callback: ((data, _env) => {
+                console.log(`------ RX`);
+
+                expect(data.reqId).toEqual(reqId);
+                expect(data.player).toBeDefined();
+
+                player = data.player;
+
+                done();
+            })
+        });
+
+        // Trigger player.create event
+        postal.publish({channel: 'player', topic: 'create.new', data: { reqId }});
+    });
+
+    test('Player can explore a system', (done) => {
+        const visibleSystemsCount = player.visibleSystems.length;
+
+        // Receive player update event
+        postal.subscribe({
+            channel: 'player', topic: 'player.updated', callback: ((data, _env) => {
+                const { playerUpd } = data;
+                expect(playerUpd.exploredSystems).toHaveLength(1);
+                expect(playerUpd.visibleSystems).not.toContain(player.exploredSystems[0]);
+                expect(playerUpd.visibleSystems.length).toBeLessThan(visibleSystemsCount + 4);
+
+                done();
+            })
+        });
+
+        // Trigger StarSystem.explore event
+        postal.publish({channel: 'StarSystem', topic: 'explore', data: { reqId }});
+    });
+
+    test.skip('Player cannot explore an unknown system', () => {
+        const sys = new StarSystem();
+
+        const block = () => {
+            // player.explore(sys.id);
+        };
+        expect(block).toThrowError(`Cannot explore system ${sys.id}: Could not find matching visible system`);
+    });
 });
 
-test('Player cannot colonise a previously colonised system', () => {
-    const player = new Player();
-    const sys = player.colonisedSystems[0];
+describe.skip('Player colonisation', () => {
+    test('Player can colonise suitable environments', () => {
+        const player = new Player();
 
-    const block = () => {
-        player.colonise(sys.id);
-    };
-    expect(block).toThrowError(`Cannot colonise system ${sys.id}: Could not find matching explored system`);
+        expect(player.canColonise(SystemEnvironment.SUITABLE)).toBe(true);
+        expect(player.canColonise(SystemEnvironment.NOT_HABITABLE)).toBe(false);
+        expect(player.canColonise(SystemEnvironment.HOSTILE)).toBe(false);
+    });
+
+    // test('Player can colonise a system', () => {
+    //     const player = new Player();
+    //
+    //     const sys = new StarSystem(SystemEnvironment.SUITABLE);
+    //     player.exploredSystems.push(sys);
+    //
+    //     player.colonise(sys.id);
+    //     expect(player.colonisedSystems).toHaveLength(2);
+    // });
+
+    // test('Player cannot colonise an unsuitable system', () => {
+    //     const player = new Player();
+    //     const sys = new StarSystem(SystemEnvironment.HOSTILE);
+    //
+    //     const block = () => {
+    //         player.exploredSystems.push(sys);
+    //         player.colonise(sys.id);
+    //     };
+    //     expect(block).toThrowError(`Cannot colonise system ${sys.id}: Not a suitable environment`);
+    // });
+
+    /**
+     * Player can only colonise systems that exist in their explored list
+     */
+    // test('Player cannot colonise an unknown system', () => {
+    //     const player = new Player();
+    //     const sys = new StarSystem(SystemEnvironment.HOSTILE);
+    //
+    //     const block = () => {
+    //         player.colonise(sys.id);
+    //     };
+    //     expect(block).toThrowError(`Cannot colonise system ${sys.id}: Could not find matching explored system`);
+    // });
+
+    // test('Player cannot colonise an unexplored system', () => {
+    //     const player = new Player();
+    //     const sys = player.visibleSystems[0];
+    //
+    //     const block = () => {
+    //         player.colonise(sys.id);
+    //     };
+    //     expect(block).toThrowError(`Cannot colonise system ${sys.id}: Could not find matching explored system`);
+    // });
+
+    // test('Player cannot colonise a previously colonised system', () => {
+    //     const player = new Player();
+    //     const sys = player.colonisedSystems[0];
+    //
+    //     const block = () => {
+    //         player.colonise(sys.id);
+    //     };
+    //     expect(block).toThrowError(`Cannot colonise system ${sys.id}: Could not find matching explored system`);
+    // });
 });
--- src/Player/Player.ts
+++ src/Player/Player.ts
@@ -23,53 +23,7 @@ export default class Player implements PlayerType {
         this.id = generateId();
     }
 
-    explore(_systemId: string): void {
-        // const targetSystem = this.visibleSystems.find((item) => systemId === item.id);
-        //
-        // if (targetSystem) {
-        //     this.exploredSystems.push(targetSystem);
-        //     console.debug(`Explored: ${targetSystem.name}`);
-        //
-        //     const newSystemCount = Math.floor(Math.random() * 5);
-        //     console.debug(`Discovered ${newSystemCount} new star systems`);
-        //
-        //     for (let i = 0; i < newSystemCount; i++) {
-        //         const s = new StarSystem();
-        //         console.debug(`Discovered: ${s.name}`);
-        //         // TODO Check for duplicate IDs
-        //         this.visibleSystems.push(s);
-        //     }
-        //
-        //     this.visibleSystems = this.visibleSystems.filter((sys) => sys !== targetSystem);
-        // } else {
-        //     throw `Cannot explore system ${systemId}: Could not find matching visible system`;
-        // }
-    }
-
     canColonise = (sysEnv: SystemEnvironment): boolean => {
         return sysEnv === SystemEnvironment.SUITABLE;
     };
-
-    colonise(_systemId: string): void {
-        // const targetSystem = this.exploredSystems.find((item) => systemId === item.id);
-        //
-        // if (targetSystem) {
-        //     if (!this.canColonise(targetSystem.environment)) {
-        //         throw `Cannot colonise system ${systemId}: Not a suitable environment`;
-        //     }
-        //
-        //     // if (targetSystem.colonyId) {
-        //     //     throw `Cannot colonise system ${systemId}: Already colonised`;
-        //     // }
-        //
-        //     // targetSystem.colony = new Colony(systemId, this.id, 100000);
-        //     // this.colonisedSystems.push(targetSystem);
-        //
-        //     console.debug(`Colonised: ${targetSystem.name}`);
-        //
-        //     this.exploredSystems = this.exploredSystems.filter((sys) => sys !== targetSystem);
-        // } else {
-        //     throw `Cannot colonise system ${systemId}: Could not find matching explored system`;
-        // }
-    }
 }
